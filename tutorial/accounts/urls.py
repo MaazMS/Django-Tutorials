9558f4a05365d7d8bc9b1fbfd4c19d53d847af0a
maazil3861 <maazshaikh7020@gmail.com>
2020-11-21 22:41:05 +0530

    templates

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
 
     path('homepage/', views.homepage),
-    path('templates/', views.templates)
+    path('templates/', views.templates),
+    path('value/', views.ginger),
+    path('home/', views.Home_extends_base),
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -7,3 +7,12 @@ def homepage(request):
 
 def templates(request):
     return render(request, 'accounts/login.html')
+
+def ginger(request):
+    numbers = [1, 2, 3, 4, 5]
+    name = 'Maaz'
+    dictionary = {'myName' : name, 'numbers' : numbers}
+    return  render(request, 'accounts/passing_value.html', dictionary )
+
+def Home_extends_base(request):
+    return render(request, 'accounts/home.html')
